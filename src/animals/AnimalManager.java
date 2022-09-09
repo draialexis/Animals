26a54ac0a41ca833b56b9291af225a26265fb708
Alexis DRAI <aldrai@iutclinfb1903l>
2022-09-09 17:17:23 +0200

    Get started on the animal manager

---
+++ src/animals/AnimalManager.java
@@ -0,0 +1,25 @@
+package animals;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AnimalManager {
+    private final List<Animal> animals;
+
+    public AnimalManager() {
+        this.animals = new ArrayList<>();
+    }
+    
+    /**
+     * Get all animals from the list
+     * @return an unmodifiable view of the list of all animals
+     */
+    public List<Animal> getAll(){
+        return Collections.unmodifiableList(this.animals);
+    }
+    
+    public void createOne(Animal newAnimal){
+        this.animals.add(newAnimal);
+    }
+}
