611c79fec97d7e6ce26147d65382acd31d1de8b9
Alexis DRAI <aldrai@iutclinfb1903l>
2022-09-09 17:18:43 +0200

    Add Okapi

---
+++ src/animals/Okapi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animals;
+
+/**
+ *
+ * @author aldrai
+ */
+public class Okapi extends Animal {
+    
+    public Okapi(String name){
+        super(name);
+    }
+    
+    public void pose(){
+        System.out.println("Okapi is posing");
+    }
+}
