62f0a4f4af04321eccb735ac29891bd99b717931
Alexis DRAI <drai.alexis@gmail.com>
2022-09-11 13:56:56 +0200

    Add validation on create and update, redefine toString

--- src/animalsApp/animals/Animal.java
+++ src/animalsApp/animals/Animal.java
@@ -11,11 +11,32 @@ package animalsApp.animals;
 public abstract class Animal {
     private String name;
     
+    protected Animal(){
+        this.name = null;
+    }
+    
     protected Animal(String name){
+        validateName(name);
         this.name = name;
     }
     
-    public void getName(){
-        System.out.println(this.name);
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String name){
+        validateName(name);        
+        this.name = name;
+    }
+    
+    private void validateName(String name) {
+        if(name == null || name.isBlank()){
+            throw new IllegalArgumentException("!!>> name cannot be null or blank");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " the " + this.getClass().getSimpleName();
     }
 }
