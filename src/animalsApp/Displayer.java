cb12f286cdc7798f59c2f2790f6a91580938d65b
Alexis DRAI <drai.alexis@gmail.com>
2022-09-11 13:55:08 +0200

    Fix user input bugs, prevent adding nulls

--- src/animalsApp/Displayer.java
+++ src/animalsApp/Displayer.java
@@ -24,57 +24,54 @@ public class Displayer {
     public void showMenu() {
         boolean isQuitting = false;
         while (!isQuitting) {
-            int choice;
-            System.out.println("0........to quit this program");
-            System.out.println("1........to register an animal");
-            System.out.println("2........to display all animals");
-            System.out.println("3........to modify an animal");
-            System.out.println("4........to delete an animal");
-            choice = scanner.nextInt();
-            switch (choice) {
-                case (1) -> {
-                    int species;
+            System.out.println();
+            switch (requestLine("Menu"
+                    + "\n0........to quit this program"
+                    + "\n1........to register an animal"
+                    + "\n2........to display all animals"
+                    + "\n3........to modify an animal"
+                    + "\n4........to delete an animal")) {
+                case ("1") -> {
                     String name;
-
-                    System.out.println("choose a species");
-                    System.out.println("0........Okapi");
-                    System.out.println("1........Ostrich");
-                    System.out.println("2........Kiwi");
-                    species = scanner.nextInt();
-
-                    System.out.println("enter name (no spaces allowed)");
-                    name = scanner.next();
-                    scanner.nextLine();
-                    if (name != null && species >= 0 && species <= 4) {
-                        Animal newAnimal;
-                        switch (species) {
-                            case (0) -> {
+                    name = requestLine("enter name");
+                    if (name != null) {
+                        Animal newAnimal = null;
+                        switch (requestLine("choose a species"
+                        + "\n0........Okapi"
+                        + "\n1........Ostrich"
+                        + "\n2........Kiwi")) {
+                            case ("0") -> {
                                 newAnimal = new Okapi(name);
                             }
-                            case (1) -> {
+                            case ("1") -> {
                                 newAnimal = new Ostrich(name);
                             }
-                            case (2) -> {
+                            case ("2") -> {
                                 newAnimal = new Kiwi(name);
                             }
+                            default -> {
+                                System.out.println("not a valid option");
+                                break;
+                            }
                         }
-                        if (newAnimal != null) {
-                            registerAnimal(newAnimal);
-                        }
+                        if(newAnimal == null) { // should never be reached
+                            break;              // should never be reached
+                        }                       // should never be reached
+                        registerAnimal(newAnimal);
                     }
                 }
-                case (2) -> {
+                case ("2") -> {
                     showAllAnimals();
                 }
-                case (3) -> {
+                case ("3") -> {
                     //...
-                    modifyAnimal(String oldName, Animal newAnimal);
+                    modifyAnimal(String oldName, String newName);
                 }
-                case (4) -> {
+                case ("4") -> {
                     //...
                     deleteAnimal(String oldName);
                 }
-                case (0) -> {
+                case ("0") -> {
                     isQuitting = true;
                 }
                 default -> {
@@ -96,11 +93,18 @@ public class Displayer {
         }
     }
 
-    private void modifyAnimal(String oldName, Animal newAnimal) {
+    private void modifyAnimal(String oldName, String newName) {
 
     }
 
     private void deleteAnimal(String oldName) {
 
+    }
+    
+    private String requestLine(String msg) {
+        String input;
+        System.out.println(msg);
+        input = scanner.nextLine();
+        return input;
     }
 }
