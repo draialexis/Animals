5fd0e871e3809e98bbce1e1c3ad00a3669f18039
Alexis DRAI <drai.alexis@gmail.com>
2022-09-11 15:05:23 +0200

    Implement CRUD in data service

--- src/animalsApp/AnimalManager.java
+++ src/animalsApp/AnimalManager.java
@@ -4,6 +4,7 @@ import animalsApp.animals.Animal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AnimalManager {
 
@@ -22,11 +23,57 @@ public class AnimalManager {
         return Collections.unmodifiableList(this.animals);
     }
 
-    public Animal getOne(String name, String type) {
-
+    private Animal getOne(String name, String type) {
+        List<Animal> candidates = this.animals
+                .stream()
+                .filter(animal -> animal.getClass().getSimpleName().equalsIgnoreCase(type)
+                && animal.getName().equalsIgnoreCase(name))
+                .collect(Collectors.toList());
+        if (!candidates.isEmpty()) {
+            return candidates.get(0);
+        } else {
+            return null;
+        }
     }
 
-    public void createOne(Animal newAnimal) {
+    public Animal addOne(Animal newAnimal) {
+        if (newAnimal == null) {
+            throw new IllegalArgumentException(
+                    "!!>> new Animal may not be null"
+            );
+        }
+        if (getOne(newAnimal.getName(), newAnimal.getClass().getSimpleName()) != null) {
+            throw new IllegalArgumentException(
+                    "!!>> you cannot name two " + newAnimal.getClass().getSimpleName()
+                    + " " + newAnimal.getName()
+            );
+        }
         this.animals.add(newAnimal);
+        return newAnimal;
+    }
+
+    public Animal updateOne(Animal oldAnimal, String newName) {
+        if (oldAnimal == null) {
+            throw new IllegalArgumentException(
+                    "!!>> oldAnimal may not be null"
+            );
+        }
+        if (getOne(newName, oldAnimal.getClass().getSimpleName()) != null) {
+            throw new IllegalArgumentException(
+                    "!!>> you cannot name two " + oldAnimal.getClass().getSimpleName()
+                    + " " + newName
+            );
+        }
+        oldAnimal.setName(newName);
+        return oldAnimal;
+    }
+
+    public void deleteOne(Animal oldAnimal) {
+        if (oldAnimal == null) {
+            throw new IllegalArgumentException(
+                    "!!>> oldAnimal may not be null"
+            );
+        }
+        this.animals.remove(oldAnimal);
     }
 }
